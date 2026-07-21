c24a5470e7022a34d04cc2eb1145e2b9f894c008
thaiholic365-cyber <thaiholic365@gmail.com>
2026-07-21 22:51:32 +0900

    Add files via upload

--- public/config.js
+++ public/config.js
@@ -1,4 +1,6 @@
+/* Supabase 연결 정보
+   ⚠️ 이 두 값은 공개되어도 안전합니다 (anon key는 RLS로 보호됨)
+   절대 service_role 키를 넣지 마세요! */
 window.TL_CONFIG = {
-  url: 'https://abcdefghijk.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwb3lhZ3Bhc3duanZkdnpjZ2l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2MjYxNDgsImV4cCI6MjEwMDIwMjE0OH0.tuyIPjctr0tuGRrei0H0rRPNWFpWZD7RvMmeXwGCnKY'
+  url: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwb3lhZ3Bhc3duanZkdnpjZ2l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2MjYxNDgsImV4cCI6MjEwMDIwMjE0OH0.tuyIPjctr0tuGRrei0H0rRPNWFpWZD7RvMmeXwGCnKY'
 };
